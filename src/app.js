8033cf179a152637877c023874b10aad79921f49
Alicia Benavente <aliciabenaventegarcia@gmail.com>
2024-07-17 17:09:22 +0000

    user inputs saved, result shown in screen

--- src/app.js
+++ src/app.js
@@ -6,52 +6,54 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronouns = ["her", "the"];
-  let adjectives = ["clumsy", "suspicious"];
-  let names = ["mother", "teacher"];
-  let domains = [".com", ".es"];
-
-  function domainGenerator(array1, array2, array3, array4) {
-    let combinations = [];
-
-    for (let str1 of array1) {
-      for (let str2 of array2) {
-        for (let str3 of array3) {
-          for (let str4 of array4) {
-            let combination = str1 + str2 + str3 + str4;
-            combinations.push(combination);
-          }
-        }
-      }
-    }
-    return combinations.join("\n");
-  }
-  let generatedDomain = domainGenerator(pronouns, adjectives, names, domains);
-  document.querySelector("#domain").innerHTML = generatedDomain;
-  console.log(generatedDomain);
+  let pronouns = [""];
+  let adjectives = [""];
+  let names = [""];
+  let domains = [""];
 
   document
     .getElementById("domainForm")
     .addEventListener("submit", function(event) {
       event.preventDefault();
 
-      const selectedPronoun = document.getElementById("pronoun").value;
-      const selectedName = document.getElementById("name").value;
-      const selectedAdjective = document.getElementById("adjective").value;
+      const pronounInput = document.getElementById("pronoun").value;
+      const adjectiveInput = document.getElementById("adjective").value;
+      const nameInput = document.getElementById("name").value;
+
+      pronouns.push(pronounInput);
+      adjectives.push(adjectiveInput);
+      names.push(nameInput);
+
       const selectedExtension = document.querySelector(
-        "input[name=extensionRadio]:checked"
+        'input[name="extensionRadio"]:checked'
       ).value;
 
-      pronouns.push(selectedPronoun);
-      names.push(selectedName);
-      adjectives.push(selectedAdjective);
       domains.push(selectedExtension);
+    });
 
+  function result() {
+    let userResult = [];
+
+    const pronounInput = document.getElementById("pronoun").value;
+    const adjectiveInput = document.getElementById("adjective").value;
+    const nameInput = document.getElementById("name").value;
+
+    userResult.push(pronounInput + adjectiveInput + nameInput);
+    const selectedExtension = document.querySelector(
+      'input[name="extensionRadio"]:checked'
+    ).value;
+
+    userResult.push(selectedExtension);
+
+    return userResult;
+  }
+  document
+    .getElementById("createDomainButton")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+
+      let userResult = result();
       const resultElement = document.getElementById("result");
-      resultElement.textContent = `Your personalize domain is: ${
-        pronouns[Math.floor(Math.random() * pronouns.length)]
-      }${names[Math.floor(Math.random() * names.length)]}${
-        adjectives[Math.floor(Math.random() * adjectives.length)]
-      }${domains[Math.floor(Math.random() * domains.length)]}`;
+      resultElement.textContent = "Your domain is: " + userResult.join("");
     });
 };
